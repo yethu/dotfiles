ab451e60e9840e3a11790c5323779207e995e377
Ye Thu <yethu.personal@gmail.com>
2021-05-19 23:04:32 +0630

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,17 @@
 fish_add_path /home/yethu/thirdparty/flutter/bin
+fish_add_path /home/yethu/thirdparty/graalvm-ce-java11-21.1.0/bin
 fish_add_path /usr/local/go/bin
 fish_add_path /home/yethu/go/bin
 fish_add_path /home/yethu/.cargo/bin
 
+set JAVA_HOME /home/yethu/thirdparty/graalvm-ce-java11-21.1.0/bin
+
+#set -gx PATH /home/yethu/thirdparty/flutter/bin $PATH
+#set -gx PATH /usr/local/go/bin $PATH
+#set -gx PATH /home/yethu/go/bin $PATH
+#set -gx PATH /home/yethu/.cargo/bin $PATH
+#---#
+# set -gx PATH /home/yethu/miniconda3/bin $PATH  # commented out by conda initialize
 bass source ~/.nvm/nvm.sh --no-use ';' nvm use node
 
 # >>> conda initialize >>>
