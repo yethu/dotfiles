7c21033c2bd0ab27da58a1b98d339b1f2410a3c3
Ye Thu <yethu.personal@gmail.com>
2021-05-23 01:53:04 +0630

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,15 @@
 fish_add_path /home/yethu/thirdparty/flutter/bin
 fish_add_path /home/yethu/thirdparty/graalvm-ce-java11-21.1.0/bin
+fish_add_path /home/yethu/thirdparty/wabt-1.0.23/bin
 fish_add_path /usr/local/go/bin
 fish_add_path /home/yethu/go/bin
 fish_add_path /home/yethu/.cargo/bin
 
 set JAVA_HOME /home/yethu/thirdparty/graalvm-ce-java11-21.1.0/bin
 
-#set -gx PATH /home/yethu/thirdparty/flutter/bin $PATH
-#set -gx PATH /usr/local/go/bin $PATH
-#set -gx PATH /home/yethu/go/bin $PATH
-#set -gx PATH /home/yethu/.cargo/bin $PATH
 #---#
 # set -gx PATH /home/yethu/miniconda3/bin $PATH  # commented out by conda initialize
-bass source ~/.nvm/nvm.sh --no-use ';' nvm use node
+bass source ~/.nvm/nvm.sh --no-use ';' nvm use node > /dev/null
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
@@ -20,3 +17,9 @@ eval /home/yethu/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 rvm default
+
+set -gx WASMTIME_HOME "$HOME/.wasmtime"
+
+#--#
+# wasmtime
+string match -r ".wasmtime" "$PATH" > /dev/null; or set -gx PATH "$WASMTIME_HOME/bin" $PATH
