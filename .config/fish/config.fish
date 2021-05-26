9fd935a0b097d799b16e438c41a14840f02c4a68
Ye Thu <yethu.personal@gmail.com>
2021-05-26 11:07:53 +0630

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_add_path /home/yethu/thirdparty/flutter/bin
 fish_add_path /home/yethu/thirdparty/graalvm-ce-java11-21.1.0/bin
 fish_add_path /home/yethu/thirdparty/wabt-1.0.23/bin
+fish_add_path /home/yethu/thirdparty/apache-maven-3.8.1/bin
 fish_add_path /usr/local/go/bin
 fish_add_path /home/yethu/go/bin
 fish_add_path /home/yethu/.cargo/bin
@@ -9,7 +10,7 @@ set JAVA_HOME /home/yethu/thirdparty/graalvm-ce-java11-21.1.0/bin
 
 #---#
 # set -gx PATH /home/yethu/miniconda3/bin $PATH  # commented out by conda initialize
-bass source ~/.nvm/nvm.sh --no-use ';' nvm use node > /dev/null
+bass source ~/.nvm/nvm.sh --no-use ';' nvm use node > /dev/null 2&>/dev/null
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
